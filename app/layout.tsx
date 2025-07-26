b98b7b25c569373d0b8480fcfebd2e0b0bfe5a15
TheKany <skarnjs06@naver.com>
2025-07-26 21:49:16 +0900

    메타태그 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,11 @@ export const viewport = {
 export const metadata = {
   title: "타로타르트",
   description: "달콤하게 맛보는 나의 운명: 타로타르트",
+  keywords: ["타로", " 타로카드", "타로점", "운세", "운명"],
+  url: "https://tarot-tart.vercel.app/",
+  siteName: "타로타르트",
+  locale: "ko_KR",
+  type: "website",
   icons: {
     icon: [
       { url: "/favicon.ico", type: "image/x-icon", sizes: "any" },
@@ -32,6 +37,28 @@ export const metadata = {
     ],
     apple: { url: "/apple-icon-180x180.png", sizes: "180x180" },
     manifest: "/manifest.json",
+  },
+  openGraph: {
+    title: "타로타르트",
+    description: "달콤하게 맛보는 나의 운명: 타로타르트",
+    url: "https://tarot-tart.vercel.app/",
+    siteName: "타로타르트",
+    images: [
+      {
+        url: "/banner.png",
+        width: 1200,
+        height: 630,
+        alt: "타로타르트 미리보기 이미지",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "타로타르트",
+    description: "달콤하게 맛보는 나의 운명: 타로타르트",
+    images: ["/banner.png"],
   },
   other: {
     "google-adsense-account": "ca-pub-4184442303277144",
@@ -49,7 +76,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4184442303277144"
